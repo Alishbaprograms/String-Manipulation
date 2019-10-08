f7bc5ae5a99cebf2ab65047797ecd661f9005860
Alishbaprograms <54945091+Alishbaprograms@users.noreply.github.com>
2019-10-08 03:47:41 -0700

    Pangram vb
    Pangram codes of visual basic

---
+++ Ispangram.vb
@@ -0,0 +1,44 @@
+﻿Module Ispangram
+
+    Sub Main()
+        Dim s1 As String
+        Dim nextchar As String
+        Dim count As Integer
+        Dim alphaset As String
+        Dim Ispangram As Boolean
+
+        s1 = ""
+        nextchar = ""
+        count = 0
+        alphaset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        Ispangram = True
+
+        Console.WriteLine("Enter you string: ")
+        s1 = Console.ReadLine
+        s1 = UCase(s1)
+
+
+        For count = 1 To Len(alphaset)
+            nextchar = Mid(alphaset, count, 1)
+            If InStr(s1, nextchar) = 0 Then
+                Ispangram = False
+                Exit For
+            End If
+        Next
+        If Ispangram = True Then
+            Console.Write("This is a pangram")
+        Else
+            Console.Write("This is not a pangram")
+
+
+        End If
+        Console.ReadKey()
+
+
+
+
+
+
+    End Sub
+
+End Module
