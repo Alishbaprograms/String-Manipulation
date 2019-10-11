5dcba79f54e6e97a2299a2bc47bcbf5619d6ee37
Alishbaprograms <54945091+Alishbaprograms@users.noreply.github.com>
2019-10-11 08:47:36 -0700

    iso_words
    Isolating words vb codes.

---
+++ iso_words.vb
@@ -0,0 +1,43 @@
+﻿Module iso_words
+
+    Sub Main()
+        Dim rawstr As String
+        Dim firstword As String
+        Dim midword As String
+        Dim finalword As String
+        Dim count As Integer
+        Dim space As Integer
+        Dim space2 As Integer
+
+        rawstr = ""
+        firstword = ""
+        midword = ""
+        finalword = ""
+        count = 0
+        space = 0
+        space2 = 0
+
+        Console.Write("Enter the string: ")
+        rawstr = Console.ReadLine
+
+        space = InStr(rawstr, " ")
+
+        firstword = Left(rawstr, space - 1)
+        space2 = InStr(space, rawstr, " ")
+
+        midword = Mid(rawstr, space + 1, space2 - 1)
+
+
+        finalword = Right(rawstr, space2 + 1)
+        Console.WriteLine(firstword)
+
+        Console.WriteLine(midword)
+        Console.WriteLine(finalword)
+
+
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
