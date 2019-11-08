9f8929957d592ff01b19b2d0856aaa65ae5a24f7
Alishbaprograms <54945091+Alishbaprograms@users.noreply.github.com>
2019-11-08 08:12:42 -0800

    Binary to denary
    binary to denary converter

---
+++ bin to den converter.vb
@@ -0,0 +1,66 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim binarystring As String
+        Dim validBinaryString As Boolean
+        Dim nextchar As Char
+        Dim binarynum As Integer
+        Dim nextnum As Integer
+        Dim denarynum As Integer
+        Dim count2 As Integer
+        Dim worth As Integer
+
+
+
+        binarystring = ""
+        validBinaryString = True
+        nextchar = ""
+        binarynum = 0
+        nextnum = 0
+        denarynum = 0
+        count2 = 0
+        worth = 0
+
+
+
+
+        Console.Write("Enter binary number: ")
+        binarystring = Console.ReadLine
+
+        validBinaryString = True
+        If Len(binarystring) < 1 And Len(binarystring) > 8 Then
+            validBinaryString = False
+        End If
+
+        For count = 1 To Len(binarystring)
+
+
+            nextchar = Mid(binarystring, count, 1)
+
+            If nextchar <> "0" And nextchar <> "1" Then
+                validBinaryString = False
+
+            End If
+        Next
+
+
+        If validBinaryString = False Then
+            Console.WriteLine("Not a valid binary number...")
+        Else
+            Console.WriteLine("A valid binary number...")
+
+        End If
+        binarynum = Int(binarystring)
+
+        For count2 = 1 To Len(binarynum)
+            worth = (Mid(binarynum, Len(binarynum) - count2 + 1, 1) * (2 ^ (count2 - 1)))
+            nextnum = nextnum + worth
+        Next
+
+        Console.WriteLine(binarystring & "=" & nextnum)
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
