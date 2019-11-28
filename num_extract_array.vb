acd1cefd44c7c7d5796f5f79d9a3ae121254f291
Alishbaprograms <54945091+Alishbaprograms@users.noreply.github.com>
2019-11-28 03:00:50 -0800

    num_extract_array.vb
    num_extract_array.vb

---
+++ num_extract_array.vb
@@ -0,0 +1,37 @@
+﻿Module array
+
+    Sub Main()
+        Dim numbers(100) As Integer
+        Dim inputstring As String
+        Dim nextchar As String
+        Dim nextnumberstring As String
+        Dim i As Integer
+        Dim j As Integer
+
+        Console.WriteLine("String...")
+        inputstring = Console.ReadLine
+        j = 1
+        nextchar = Mid(inputstring, j, 1)
+
+        i = 1
+        While nextchar <> "#"
+            nextnumberstring = ""
+            While nextchar <> "*"
+                nextnumberstring = nextnumberstring & nextchar
+                j = j + 1
+                nextchar = Mid(inputstring, j, 1)
+            End While
+
+            numbers(i) = Val(nextnumberstring)
+            i = j + 1
+            j = j + 1
+            nextchar = Mid(inputstring, j, 1)
+
+        End While
+        For i = 1 To 10
+            Console.WriteLine(numbers(i))
+        Next
+        Console.ReadKey()
+    End Sub
+
+End Module
