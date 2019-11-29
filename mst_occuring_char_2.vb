13d2102c6e3f2e10d5748c13a5a35a9ff4814ad5
Alishbaprograms <54945091+Alishbaprograms@users.noreply.github.com>
2019-11-29 00:28:14 -0800

    most_occurring_char_2.vb
    most occurring char 2 vb codes

---
+++ mst_occuring_char_2.vb
@@ -0,0 +1,71 @@
+﻿Module mst_occuring_char_2
+
+    Sub Main()
+        Dim mystr1 As String
+        Dim mychar1 As String
+        Dim mychar2 As String
+        Dim count As Integer
+        Dim count2 As Integer
+        Dim counter1 As Integer
+        Dim h As Integer
+        Dim finalchar As String
+        Dim Isemptystr As Boolean
+        Dim norepeatedchar As Boolean
+
+        mystr1 = ""
+        mychar1 = ""
+        mychar2 = ""
+        count = 0
+        count2 = 0
+        finalchar = ""
+        Isemptystr = False
+        norepeatedchar = False
+
+
+        Console.WriteLine("Enter your string: ")
+        mystr1 = Console.ReadLine
+
+        If Len(mystr1) = 0 Then
+            Isemptystr = True
+
+
+        End If
+
+        For count = 1 To Len(mystr1)
+            mychar1 = Mid(mystr1, count, 1)
+            counter1 = 0
+
+            For count2 = 1 To Len(mystr1)
+                mychar2 = Mid(mystr1, count2, 1)
+                If mychar1 = mychar2 Then
+                    counter1 = counter1 + 1
+
+                End If
+            Next
+
+
+            If counter1 > h And norepeatedchar = False Then
+                h = counter1
+                finalchar = mychar1
+
+            End If
+        Next
+        If Isemptystr = True Then
+            Console.WriteLine("you have an empty string")
+
+        Else
+
+            Console.WriteLine(finalchar & " is repeated " & h & " times")
+
+        End If
+        If h = 1 Then
+            norepeatedchar = True
+            Console.WriteLine("no repeatedchar")
+        End If
+
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
