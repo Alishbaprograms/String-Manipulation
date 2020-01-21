fa179cae51e437d8f1ff5af6c95383ebde2d021b
Alishbaprograms <54945091+Alishbaprograms@users.noreply.github.com>
2020-01-21 17:45:21 +0500

    Membership.vb
    Filing. Mode:OUTPUT

---
+++ Membership.vb
@@ -0,0 +1,40 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim mtotal As Integer
+        Dim name As String
+        Dim Id As Integer
+        Dim telenum As Integer
+        Dim Isfound As Boolean
+        Dim more As String
+
+        mtotal = 0
+        Id = 0
+        telenum = 0
+        Isfound = False
+        name = ""
+        more = ""
+
+        FileOpen(1, "MemberShip.txt", OpenMode.Output)
+
+        Do
+            WriteLine(1, name)
+            WriteLine(1, Id)
+
+            Console.Write("Enter name: ")
+            name = Console.ReadLine
+
+            Console.Write("Enter ID: ")
+            Id = Console.ReadLine
+
+
+            Console.WriteLine("Do u want to do it again? ")
+            more = Console.ReadLine
+
+        Loop Until more = "n" Or more = "N" Or more = "No" Or more = "no" Or more = "NO"
+        FileClose()
+
+        Console.ReadKey()
+    End Sub
+
+End Module
