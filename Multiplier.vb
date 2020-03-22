97dd712eaddfd87d1001de8da435234531c74a89
Alishbaprograms <54945091+Alishbaprograms@users.noreply.github.com>
2020-03-22 18:28:13 +0500

    Multiplier
    Times table

---
+++ Multiplier.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim a As Integer
+        Dim b As Integer
+        Dim c As Integer
+
+        a = 0
+        b = 0
+        c = 0
+        Console.WriteLine("Enter the tabe you want to learn: ")
+        b = Console.ReadLine
+        Console.WriteLine()
+
+
+        For a = 1 To 10
+            c = b * a
+            Console.WriteLine(b & "*" & a & "=" & c)
+
+        Next
+        Console.ReadLine()
+
+    End Sub
+
+End Module
